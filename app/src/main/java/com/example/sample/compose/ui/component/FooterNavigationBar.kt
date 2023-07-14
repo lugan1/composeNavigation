715b768a494b35ee659289bf9ea208a48628f24d
이후기 <hglee@softnet.co.kr>
2023-07-14 13:15:42 +0900

    * 아이콘 추가 * 코드 리팩토링

--- app/src/main/java/com/example/sample/compose/ui/component/FooterNavigationBar.kt
+++ app/src/main/java/com/example/sample/compose/ui/component/FooterNavigationBar.kt
@@ -1,37 +1,43 @@
 package com.example.sample.compose.ui.component
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.sample.compose.R
 
-@OptIn(ExperimentalMaterial3Api::class)
+sealed class BottomScreen(val route: String, val label: String, val iconRes: Int) {
+    object FirstScreen: BottomScreen("first", "홈", R.drawable.ic_home)
+    object SecondScreen: BottomScreen("second","블루투스", R.drawable.ic_bluteooth)
+    object ForthScreen: BottomScreen("forth", "세팅", R.drawable.ic_setting)
+
+    companion object {
+        val navItems = listOf(FirstScreen, SecondScreen, ForthScreen)
+    }
+}
+
+
 @Composable
 fun FooterNavigationBar(navController: NavController) {
-    val navItems = listOf("first", "second", "forth")
-    var selectedItemIndex by remember { mutableStateOf(0) }
-
     NavigationBar {
-        navItems.forEachIndexed { index, item ->
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        BottomScreen.navItems.forEach { bottomScreen ->
             NavigationBarItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                label = { Text(item) },
-                selected = selectedItemIndex == index,
+                icon = { Icon(painter = painterResource(id = bottomScreen.iconRes), contentDescription = bottomScreen.label) },
+                label = { Text(bottomScreen.route) },
+                selected = currentDestination?.hierarchy?.any { it.route == bottomScreen.route } == true,
                 onClick = {
-                    selectedItemIndex = index
-                    navController.navigate(item) {
+                    navController.navigate(bottomScreen.route) {
                         //그래프의 시작 지점까지 팝업하여, 사용자가 항목을 선택함에 따라 백 스택에 대량의 항목이 쌓이지 않도록 방지한다.
                         popUpTo(navController.graph.findStartDestination().id) { saveState = true }
 
@@ -45,6 +51,8 @@ fun FooterNavigationBar(navController: NavController) {
         }
     }
 }
+
+
 
 @Preview
 @Composable
